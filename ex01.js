fd772a9bdd822700fb24bf605e280da647e5436f
Sheron <103598166+shemaciel@users.noreply.github.com>
2023-06-17 13:46:48 -0300

    ajuste
    fiz ajustes nos botões nas cores e nas funções

--- ex01.js
+++ ex01.js
@@ -2,21 +2,25 @@
  let armazenando1 = 0;
  let armazenando2 = 0;
 
-let colocado = 0;
-let colocado1 = 0;
-let colocado2 = 0;
+ let colocado = 0;
+ let colocado1 = 0;
+ let colocado2 = 0;
+
+ let resultado0  = 7;
+ let resultado1 = 8; 
+ let resultado2 = 9;
+
+ let total = 0;
+ let total1 = 0;
+ let total2 = 0;
 
 function enchendoAguaF() {
     let qntInput = Number(document.getElementById("iquantidadeDeAguaF").value);
-        resultado = qntInput + armazenando; 
-        armazenando = resultado; 
-        document.getElementById("quantidadeF").innerHTML = resultado;
+        resultado0 = qntInput + armazenando; 
+        armazenando = resultado0; 
+        document.getElementById("quantidadeF").innerHTML = resultado0;
         totalLitros()
-      
-     
-
  }
-
  function enchendoAguaS(){
    let qntInput1 = Number(document.getElementById('iquantidadeDeAguaS').value);
        resultado1 = qntInput1 + armazenando1; 
@@ -24,94 +28,123 @@ function enchendoAguaF() {
        document.getElementById("quantidadeS").innerHTML = resultado1;
        totalLitros()
     }
-
     function enchendoAguaM(){
     let qntInput2 = Number(document.getElementById('iquantidadeDeAguaM').value);
         resultado2 = armazenando2 + qntInput2; 
         armazenando2 = resultado2;  
         document.getElementById("quantidadeM").innerHTML = resultado2;
         totalLitros()
-
-
-        
     }
     function totalLitros() {
-        let somar = resultado + resultado1 + resultado2;
+        let somar = resultado0 + resultado1 + resultado2;
         window.document.getElementById("itotalLitros").innerHTML = somar;
     }
-
-
-
-
-    function ResumoA(){
-
-        maiorn = Math.max(resultado, resultado1, resultado2);  
-        document.getElementById(resultado);
-        menorn = Math.min(resultado, resultado1, resultado2);
-        document.getElementById("maior".value) = maiorn;
-	    document.getElementById("menor".value) = menorn;
-
-
-    }
-
-
     function enchendoRacaoF(){
-       
         let potinho = Number(document.getElementById('iquantidadeDeRacaoF').value);
         total = potinho + colocado;
         colocado = total;
         document.getElementById("cheioF").innerHTML = total;
-        console.log("essa Total  ",total)
+        console.log(total)
         totalRacao()
     }
-
-
     function enchendoRacaoS(){
-        
         let potinho1 = Number(document.getElementById('iquantidadeDeRacaoS').value);
         total1 = potinho1 + colocado1;
         colocado1 = total1;
         document.getElementById("cheioS").innerHTML = total1;
-        console.log("essa Total 1 ",total1)
+        console.log(total1)
         totalRacao()
     }
-
     function enchendoRacaoM(){
         
         let potinho2 = Number(document.getElementById('iquantidadeDeRacaoM').value);
         total2 = potinho2 + colocado2;
         colocado2 = total2;
         document.getElementById("cheioM").innerHTML = total2;
-        console.log("essa Total 2 ",total2)
+        console.log(total2)
         totalRacao()
-       
+    }
+    function totalRacao() {
+       let some = total + total1 + total2;
+        window.document.getElementById("itotalRacao").innerHTML = some
+    }
+    function menorQuantidadeA() {
+        let menorFer = 'Fernando';
+        let menorShe = 'Sheron';
+        let menorMig = 'Miguel';
+        let encheuIgualA = 'Valor Igual'
 
-        
+    if (resultado0 < resultado1 && resultado0 < resultado2){      console.log('Fernando encheu menos água');
+    document.getElementById ('menorLitro').innerHTML = 'Fernando';
+    } 
+    else if (resultado1 < resultado0 && resultado1 < resultado2){ console.log('Sheron encheu menos água');
+    document.getElementById('menorLitro').innerHTML = 'Sheron';
+    } 
+    else if (resultado2 < resultado0 && resultado2 < resultado1){ console.log('Miguel encheu menos água');
+    document.getElementById('menorLitro').innerHTML = 'Miguel';
+    }
+    else {
+    console.log('Encheram quantidades igual');
+    document.getElementById('menorLitro').innerHTML = 'Valor Igual';
     }
 
-    function totalRacao() {
-        //let some = 0;
-        some = total + total1 + total2;
-        window.document.getElementById("itotalRacao").innerHTML = some
+    }
+    function menorQuantidadeR(){
+        let colocouMenosF = 'Fernando'
+        let colocouMenosS = 'Sheron'
+        let colocouMenosM = 'Miguel'
+        let encheuIgualR = 'Valor Igual'
+    if (total < total1 && total < total2){
+    console.log('Fernndo colocou menos ração');
+    document.getElementById('menorqnt').innerHTML = 'Fernando';
+}
+    else if (total1 < total && total1 < total2){
+        console.log('Sheron colocou menos ração');
+        document.getElementById('menorqnt').innerHTML = 'Sheron';
+    }
+    else if (total2 < total && total2 < total1){
+        console.log('Miguel colocou menos ração');
+        document.getElementById('menorqnt').innerHTML = 'Miguel';
+    }
+    else {
+        console.log('Encheram quantidades igual');
+        document.getElementById('menorqnt').innerHTML = 'Valor Igual';
+        }
+    
 
-       console.log("VARIAVEL SOME",some)
-        console.log("essa Total  ",total)
-        console.log("essa Total 1 ",total1)
-        console.log("essa Total 2 ",total2)
+    }
+
+       
+    
 
    
-    }
-
-    function galao(){
-
-        let maiorn = Math.max(resultado, resultado1, resultado2);  
-        let menorn = Math.min(resultado, resultado1, resultado2);
       
-        document.getElementById("maior").value = maiorn;
-		document.getElementById("menor").value = menorn;
+   
 
-      
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //var vel = 80.5
+    // console.log(`A velocidade do seu carro é de  ${vel} km/h`)
+    // if (vel > 60) {
+       // console.log(`voce ultrapassou a velocidade`)
+    // }
+   //  console.log(`Dirija sempre com o cinto de segurança`)
 
 
 
---
+++ nota.js
@@ -0,0 +1,46 @@
+ let idade = 15
+ if (idade>=18){
+    console.log('Você é adulto')
+ } else if(idade >=15){
+    console.log('Você é um adolecente')
+ }else {
+    console.log('Você não é Adulto!')
+ }
+
+       
+    
+
+   
+      
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //var vel = 80.5
+    // console.log(`A velocidade do seu carro é de  ${vel} km/h`)
+    // if (vel > 60) {
+       // console.log(`voce ultrapassou a velocidade`)
+    // }
+   //  console.log(`Dirija sempre com o cinto de segurança`)
+
+
+
+
+    
+
